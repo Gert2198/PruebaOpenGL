6283c7c4860af89d95b2d0219e259ec55eb5a8c2
Gert2198 <sergiolmblizzard@gmail.com>
2024-06-19 15:37:01 +0200

    VertexBuffers, generating, binding and putting data

--- src/main.cpp
+++ src/main.cpp
@@ -30,42 +30,6 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowSize(window, 800, 600);
 }
 
-// typedef struct Vertex
-// {
-//     vec2 pos;
-//     vec3 col;
-// } Vertex;
- 
-// static const Vertex vertices[3] =
-// {
-//     { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-//     { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-//     { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
-// };
- 
-// static const char* vertex_shader_text =
-//     "#version 330\n"
-//     "uniform mat4 MVP;\n"
-//     "in vec3 vCol;\n"
-//     "in vec2 vPos;\n"
-//     "out vec3 color;\n"
-//     "void main()\n"
-//     "{\n"
-//     "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-//     "    color = vCol;\n"
-//     "}\n"
-// ;
- 
-// static const char* fragment_shader_text =
-//     "#version 330\n"
-//     "in vec3 color;\n"
-//     "out vec4 fragment;\n"
-//     "void main()\n"
-//     "{\n"
-//     "    fragment = vec4(color, 1.0);\n"
-//     "}\n"
-// ;
-
 
 int main()
 {
@@ -76,11 +40,6 @@ int main()
         cout << "Failed to initialize GLFW" << endl;
         return -1;
     }
-
-    // glfwWindowHint(GLFW_SAMPLES, 4);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
     window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
@@ -101,78 +60,40 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
-    // glfwSwapInterval(1);
+    cout << glGetString(GL_VERSION) << endl;
 
-    // // Me creo un buffer para los vértices
-    // GLuint vertex_buffer;
-    // glGenBuffers(1, &vertex_buffer);
-    // glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
-    // // Creo un shader. TODO: ENTENDER ESTO
-    // // Creo que es como un sitio donde almacenas la info que tienes en los textos de arriba, en este almacenas el primero
-    // const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    // glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    // glCompileShader(vertex_shader);
- 
-    // // Creo otro shader? Pero fragmentado?
-    // // En este almacenas el segundo texto
-    // const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    // glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    // glCompileShader(fragment_shader);
- 
-    // // Aqui creo el programa? y le meto los shaders de antes
-    // // TODO: No se para que sirve link
-    // const GLuint program = glCreateProgram();
-    // glAttachShader(program, vertex_shader);
-    // glAttachShader(program, fragment_shader);
-    // glLinkProgram(program);
- 
-    // // TODO: Esto tampoco lo entiendo
-    // const GLint mvp_location = glGetUniformLocation(program, "MVP");
-    // const GLint vpos_location = glGetAttribLocation(program, "vPos");
-    // const GLint vcol_location = glGetAttribLocation(program, "vCol");
- 
-    // // Aqui creamos un array de vertices?
-    // // Y le metemos los dos atributos de arriba
-    // // TODO: Esto tampoco lo entiendo
-    // GLuint vertex_array;
-    // glGenVertexArrays(1, &vertex_array);
-    // glBindVertexArray(vertex_array);
-    // glEnableVertexAttribArray(vpos_location);
-    // glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, pos)); 
-    // glEnableVertexAttribArray(vcol_location);
-    // glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, col));
+    glViewport(0, 0, 800, 600);
+
+    // Creo el buffer
+    unsigned int buffer;
+    // Lo creo de verdad (no sabemos qué longitud tiene)
+    glGenBuffers(1, &buffer);
+    // Lo seleccionamos para hacer cosas con él
+    // Le decimos que es un array normal
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    // Ahora queremos meterle info
+    float positions[6] = { 
+        -0.5f, -0.5f,
+         0.0f,  0.5f, 
+         0.5f, -0.5f
+    };
+    // Aqui especificamos la longitud de nuestro buffer
+    // Hacemos 6 * sizeof(float) porque tenemos que almacenar 6 posiciones
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
 
     while(!glfwWindowShouldClose(window))
     {
-        // int width, height;
-        // glfwGetFramebufferSize(window, &width, &height);
-        // const float ratio = width / (float) height;
-
-        // glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
- 
-        // mat4x4 m, p, mvp;
-        // // m es la identidad
-        // // p es la matriz ortonormal
-        // // mvp no se lo que es la verdad
-        // mat4x4_identity(m);
-        // mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        // mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        // mat4x4_mul(mvp, p, m);
 
-        // glUseProgram(program);
-        // glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
-        // glBindVertexArray(vertex_array);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBegin(GL_TRIANGLES);
+        // glVertex2f(-0.5f, -0.5f);
+        // glVertex2f(0.0f, 0.5f);
+        // glVertex2f(0.5f, -0.5f);
+        // glEnd();
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
