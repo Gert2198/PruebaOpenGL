3fcbbc1e1ab25a057655833383b0942bd04a13b2
Gert2198 <sergiolmblizzard@gmail.com>
2024-06-20 20:41:21 +0200

    Minor issues, not relevant commit

--- src/main.cpp
+++ src/main.cpp
@@ -61,8 +61,7 @@ static ShaderProgramSource getShaderContentSingleFile(const string &path) {
 
     return { ss[0].str(), ss[1].str() };
 }
-
-string getShaderContent(string &path) {
+static string getShaderContent(string &path) {
     ifstream file(path);
     string str;
     string content;
@@ -80,14 +79,14 @@ void SLM_toggleFullscreen(GLFWwindow* window) {
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
     const GLFWvidmode* mode = glfwGetVideoMode(monitor);
     if (!fullscreen) {
-            glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
-            fullscreen = true;
-        } else {
-            int x = (mode->width - DEFAULT_WIDTH)/2;
-            int y = (mode->height - DEFAULT_HEIGHT)/2;
-            glfwSetWindowMonitor(window, NULL, x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT, mode->refreshRate);
-            fullscreen = false;
-        }
+        glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        fullscreen = true;
+    } else {
+        int x = (mode->width - DEFAULT_WIDTH)/2;
+        int y = (mode->height - DEFAULT_HEIGHT)/2;
+        glfwSetWindowMonitor(window, NULL, x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT, mode->refreshRate);
+        fullscreen = false;
+    }
 }
 void SLM_centerWindow(GLFWwindow* window) {
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
@@ -102,18 +101,22 @@ void SLM_setWindowCallbacks(GLFWwindow* window) {
 }
 
 // Callback functions
-void error_callback(int error, const char* description)
-{
+void error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error: %s\n", description);
 }
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     if (key == GLFW_KEY_ENTER && action == GLFW_PRESS) 
-        if (!fullscreen) glfwSetWindowSize(window, 600, 300);
+        if (!fullscreen) {
+            glfwSetWindowSize(window, 600, 300);
+            SLM_centerWindow(window);
+        }
     if (key == GLFW_KEY_ENTER && action == GLFW_RELEASE) 
-        if (!fullscreen) glfwSetWindowSize(window, 800, 600);
+        if (!fullscreen) {
+            glfwSetWindowSize(window, 800, 600);
+            SLM_centerWindow(window);
+        }
     if (key == GLFW_KEY_F11 && action == GLFW_PRESS) {
         SLM_toggleFullscreen(window);
     }
