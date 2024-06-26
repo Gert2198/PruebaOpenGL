f0a1a4939ddf271fb39163afb680ee6cf4dd342b
Gert2198 <sergiolmblizzard@gmail.com>
2024-06-26 15:49:36 +0200

    GIRA GIRA GIRA

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,8 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(0);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         cout << "Failed to initialize GLAD" << endl;
         return -1;
@@ -73,66 +75,40 @@ int main() {
 
         Square square(center, texture.getWidth(), texture.getHeight(), vao, layout);
 
-        // float positions[] = {
-        //     square.getVertex(0).x,    square.getVertex(0).y,    0.0f, 0.0f, // 0
-        //     square.getVertex(1).x,    square.getVertex(1).y,    1.0f, 0.0f, // 1
-        //     square.getVertex(2).x,    square.getVertex(2).y,    1.0f, 1.0f, // 2
-        //     square.getVertex(3).x,    square.getVertex(3).y,    0.0f, 1.0f  // 3
-        // };
-
-        // unsigned int indices[] = {
-        //     0, 1, 2, 
-        //     2, 3, 0
-        // };
-
         GLDebug(glEnable(GL_BLEND));
         GLDebug(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        // VertexBuffer vbo(positions, 4 * 4 * sizeof(float));
-
-        
-        // vao.addBuffer(vbo, layout);
-
-        // IndexBuffer ibo(indices, 6);
-
 
         // OJO: la matrix ortho es como un plano, sin perspectiva. No hay lejos ni cerca, los puntos no se intersecan en el plano del infinito. 
         // NO HAY PLANO DEL INFINITO, los puntos proyectivos son paralelos entre si
         glm::mat4 projMatrix = glm::ortho(0.0f, DEFAULT_WIDTH_F, 0.0f, DEFAULT_HEIGHT_F, -1.0f, 1.0f);
 
-        // La view matrix hace de "camara", es decir, si quieres que la escena se mueva para la derecha, tendras que mover la camara para la izquierda
-        // Basicamente todo va invertido
-        // Puedes hacer traslaciones, rotaciones, zoom-in y zoom-out (escalar) y todas las cosas tipicas que se pueden hacer con matrices
-        // En este caso, como la traslacion es para la izquierda, nuestra escena se moverá a la derecha
-        // MENTIRA, EMOSIDO ENGAÑADO!!!! La direccion hacia donde lo mueves es EXACTAMENTE hacia donde se mueve la escena
+        // La direccion hacia donde lo mueves es EXACTAMENTE hacia donde se mueve la escena
         // En este caso, como la traslacion es para la izquierda, LA ESCENA SE MUEVE A LA IZQUIERDA
         // Lo que está invertido es el movimiento DE LA CAMARA!! (nuestra view matrix)
         // glm::mat4 viewMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-
-        glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(center.x, center.y, 0)) 
-                                * glm::rotate(glm::mat4(1.0f), glm::quarter_pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f))
-                                * glm::translate(glm::mat4(1.0f), glm::vec3(-center.x, -center.y, 0));
-
-        glm::mat4 mvp = projMatrix * modelMatrix;
 
         Shader basicShader(basicShaderPath);
         basicShader.bind();
         basicShader.setUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
         basicShader.setUniform1i("u_Texture", 0);
-        basicShader.setUniformMat4f("u_MVP", mvp);
+        
 
         Renderer renderer;
 
-        // float r;
-        // double currentTime;
+        double currentTime;
 
         while(!glfwWindowShouldClose(window)) {
             renderer.clear();
 
-            // currentTime = glfwGetTime();
-            // r = (sin(currentTime) + 1) / 2;
+            currentTime = glfwGetTime();
 
-            // basicShader.setUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(center.x, center.y, 0)) 
+                                    * glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f))
+                                    * glm::translate(glm::mat4(1.0f), glm::vec3(-center.x, -center.y, 0));
+
+            glm::mat4 mvp = projMatrix * modelMatrix;
+
+            basicShader.setUniformMat4f("u_MVP", mvp);
 
             renderer.drawSquare(square, vao, basicShader);
 
